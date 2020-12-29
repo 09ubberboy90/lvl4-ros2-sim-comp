702f80a6a46d21341a65bb15f98ff3d6fc16ac05
09ubberboy90 <33180714+09ubberboy90@users.noreply.github.com>
2020-12-29 17:01:13 +0100

    Fixed vr publish to pub on each topic

--- simple_arm/simple_arm/vr_publish.py
+++ simple_arm/simple_arm/vr_publish.py
@@ -6,22 +6,21 @@ from simple_arm import utils
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Pose, Point, Quaternion
-import collections
+from collections import defaultdict 
 
 class VrPublisher(Node):
 
     def __init__(self, openvr_system):
         super().__init__('vr_publisher')
-        self.publisher = self.create_publisher(Pose, 'VrPose', 10)     # CHANGE
-        timer_period = 2
+        timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.devices = collections.defaultdict(list)
+        self.devices = defaultdict(list)
         self.system = openvr_system
         self.poses = []
+        self.publishers_dict = {}
 
     def timer_callback(self):
         self.poses = self.system.getDeviceToAbsoluteTrackingPose(openvr.TrackingUniverseStanding,0,self.poses)
-        print("=================")
         ########
         # # ETrackedDeviceClass = ENUM_TYPE
         # # TrackedDeviceClass_Invalid = ENUM_VALUE_TYPE(0)
@@ -32,31 +31,46 @@ class VrPublisher(Node):
         # # TrackedDeviceClass_DisplayRedirect = ENUM_VALUE_TYPE(5)
         # # TrackedDeviceClass_Max = ENUM_VALUE_TYPE(6)
         ########
+       	# # TrackedControllerRole_LeftHand = 1,					// Tracked device associated with the left hand
+        # # TrackedControllerRole_RightHand = 2,				// Tracked device associated with the right hand
+        ########
         for idx, controller in enumerate(self.poses):
             ## Needed as the order of the devices may change ( based on which thing got turned on first)
-            if self.system.getTrackedDeviceClass(idx) == 1:
-                self.devices["hmd"].append(controller)
-            elif self.system.getTrackedDeviceClass(idx) == 2:
-                self.devices["controller"].append(controller)
-        
-        pose = utils.convert_to_quaternion(self.devices["controller"][0].mDeviceToAbsoluteTracking)
+            if not self.system.isTrackedDeviceConnected(idx):
+                continue
+            if self.system.getTrackedDeviceClass(idx) == 1 and len(self.devices["hmd"]) <= 1:
+                self.devices["hmd"].append(("hmd", controller))
+            elif self.system.getTrackedDeviceClass(idx) == 2 and len(self.devices["controller"]) <= 2:
+                controller_role = self.system.getControllerRoleForTrackedDeviceIndex(idx)
+                hand = ""
+                if (controller_role ==1):
+                    hand = "LeftHand"
+                if controller_role == 2:
+                    hand = "RightHand"
+                self.devices["controller"].append((hand, controller))
+        for key, device in self.devices.items():
+            for idx, (name, el) in enumerate(device):
+                pose = utils.convert_to_quaternion(el.mDeviceToAbsoluteTracking)
+                point = Point()
+                point.x = pose[0][0]
+                point.y = pose[0][1]
+                point.z = pose[0][2]
 
-        point = Point()
-        point.x = pose[0][0]
-        point.y = pose[0][1]
-        point.z = pose[0][2]
+                rot = Quaternion()
+                rot.x = pose[1][0]
+                rot.y = pose[1][1]
+                rot.z = pose[1][2]
+                rot.w = pose[1][3]
 
-        rot = Quaternion()
-        rot.x = pose[1][0]
-        rot.y = pose[1][1]
-        rot.z = pose[1][2]
-        rot.w = pose[1][3]
-
-        msg = Pose()   
-        msg.orientation = rot
-        msg.position = point
-        self.publisher.publish(msg)
-        self.get_logger().info(f"x:{point.x}, y: {point.y}, z:{point.z}")
+                msg = Pose()   
+                msg.orientation = rot
+                msg.position = point
+                name = f"{key}/{name}"
+                pub = self.publishers_dict.get(name)
+                if pub is None:
+                    pub = self.create_publisher(Pose, name, 10)
+                    self.publishers_dict[name] = pub
+                pub.publish(msg)
 
 
 def main(args=None):
