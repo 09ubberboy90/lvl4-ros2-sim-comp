2145d9dd45928756325dc26f136a33da4de6e61c
09ubberboy90 <2330834a@student.gla.ac.uk>
2021-01-16 17:52:06 +0100

    Removed mimic as it's not implemented yet Source : https://github.com/erdalpekel/panda_simulation/issues/20#issuecomment-691627697

--- simple_move_group/launch/run_move_group.launch.py
+++ simple_move_group/launch/run_move_group.launch.py
@@ -28,10 +28,10 @@ def load_yaml(package_name, file_path):
 def generate_launch_description():
 
     # planning_context
-    robot_description_config = load_file('moveit_resources_panda_description', 'urdf/panda.urdf')
+    robot_description_config = load_file('ur_configs', 'urdf/panda/panda.urdf')
     robot_description = {'robot_description' : robot_description_config}
 
-    robot_description_semantic_config = load_file('moveit_resources_panda_moveit_config', 'config/panda.srdf')
+    robot_description_semantic_config = load_file('simple_move_group', 'config/panda.srdf')
     robot_description_semantic = {'robot_description_semantic' : robot_description_semantic_config}
 
     kinematics_yaml = load_yaml('moveit_resources_panda_moveit_config', 'config/kinematics.yaml')
