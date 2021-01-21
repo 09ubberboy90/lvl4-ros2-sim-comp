8f8467eb0ea8f3788a6f687712599bdfcb5a57f0
09ubberboy90 <2330834a@student.gla.ac.uk>
2021-01-21 19:17:47 +0100

    Removed models from list

--- simple_arm_controller/src/moveit_controller.cpp
+++ simple_arm_controller/src/moveit_controller.cpp
@@ -1,24 +1,27 @@
-# include "moveit_controller.hpp"
+#include "moveit_controller.hpp"
 
-void namer(std::shared_ptr<gazebo_msgs::srv::GetEntityState_Request> request, std::string arg) { 
-    request->name = arg; 
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), request->name);
+void namer(std::shared_ptr<gazebo_msgs::srv::GetEntityState_Request> request, std::string arg)
+{
+    request->name = arg;
 }
 void namer(std::shared_ptr<gazebo_msgs::srv::GetModelList_Request> request, std::string arg) {}
 
 void result_handler(std::shared_future<std::shared_ptr<gazebo_msgs::srv::GetModelList_Response>> result, geometry_msgs::msg::PoseArray *poses)
 {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), std::to_string(result.get()->model_names.size()));
+    std::set<std::string> banned {"panda", "ground_plane"};
     for (auto name : result.get()->model_names)
     {
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), name);
-        service_caller<gazebo_msgs::srv::GetEntityState>("get_entity_state", poses, name);
+        if (banned.count(name) == 0)
+        {
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), name);
+            service_caller<gazebo_msgs::srv::GetEntityState>("get_entity_state", poses, name);
+        }
+        
     }
 }
 
 void result_handler(std::shared_future<std::shared_ptr<gazebo_msgs::srv::GetEntityState_Response>> result, geometry_msgs::msg::PoseArray *poses)
 {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), std::to_string(result.get()->state.pose.position.x));
     poses->poses.push_back(result.get()->state.pose);
 }
 
@@ -49,33 +52,19 @@ int main(int argc, char **argv)
         action_node_name = "/follow_joint_trajectory";
     }
     auto server = std::make_shared<sim_action_server::ActionServer>("trajectory_control", action_node_name);
-    std::vector<std::string> joint_names = {"panda_joint1",
-                                            "panda_joint2",
-                                            "panda_joint3",
-                                            "panda_joint4",
-                                            "panda_joint5",
-                                            "panda_joint6",
-                                            "panda_joint7"};
     // Planning to a Pose goal
 
     if (move_group_node->get_parameter("use_spawn_obj", use_spawn_obj))
     {
-        //     moveit_msgs::msg::CollisionObject collision_object[result.get()->poses.poses.size()];
-        //     for (int i=0; i< result.get()->poses.poses.size(); i++)
-        //     {
-        //     }
-
-        // }
-
         geometry_msgs::msg::PoseArray poses;
         service_caller<gazebo_msgs::srv::GetModelList>("get_model_list", &poses);
         moveit_msgs::msg::CollisionObject collision_object[poses.poses.size()];
 
-        for (int i=0; i< poses.poses.size(); i++)
+        for (int i = 0; i < poses.poses.size(); i++)
         {
             auto pose = poses.poses[i];
             collision_object[i].header.frame_id = move_group.getPlanningFrame();
-            collision_object[i].id = "Box"+i;
+            collision_object[i].id = "Box" + i;
             shape_msgs::msg::SolidPrimitive primitive;
             primitive.type = primitive.BOX;
             primitive.dimensions.resize(3);
@@ -90,39 +79,38 @@ int main(int argc, char **argv)
                 primitive.dimensions[0] = 0.05;
                 primitive.dimensions[1] = 0.05;
                 primitive.dimensions[2] = 0.05;
-
+            }
+            if (i == 1)
+            {
+                move_group.setPoseTarget(pose);
             }
 
             // Define a pose for the box (specified relative to frame_id)
             geometry_msgs::msg::Pose box_pose;
             box_pose.orientation = pose.orientation;
             box_pose.position = pose.position;
-            if (i == 0) // table
-            {
-                box_pose.position.z += 0.45;
-            }
+
             collision_object[i].primitives.push_back(primitive);
             collision_object[i].primitive_poses.push_back(box_pose);
             collision_object[i].operation = collision_object[i].ADD;
 
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Add an object into the world");
             planning_scene_interface.applyCollisionObject(collision_object[i]);
-
         }
 
         // Now, we call the planner to compute the plan and visualize it.
         // Note that we are just planning, not asking move_group
         // to actually move the robot.
-        // moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
-        // bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-        // RCLCPP_INFO(server->get_logger(), "Plan 1 (pose goal) %s", success ? "SUCCEEDED" : "FAILED");
+        bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+        RCLCPP_INFO(server->get_logger(), "Plan 1 (pose goal) %s", success ? "SUCCEEDED" : "FAILED");
 
-        // if (success)
-        // {
-        //     std::thread([&move_group]() { move_group.asyncMove(); }).detach();
-        //     server->execute_plan(my_plan.trajectory_.joint_trajectory);
-        // }
+        if (success)
+        {
+            std::thread([&move_group]() { move_group.asyncMove(); }).detach();
+            server->execute_plan(my_plan.trajectory_.joint_trajectory);
+        }
     }
 
     rclcpp::shutdown();
