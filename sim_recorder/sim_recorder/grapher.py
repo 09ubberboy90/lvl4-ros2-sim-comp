59b375ed46b2e2f80a33c7e4475b1404cefd0bdf
09ubberboy90 <2330834a@student.gla.ac.uk>
2021-02-10 18:32:21 +0100

    added smoothing and variance

--- sim_recorder/sim_recorder/grapher.py
+++ sim_recorder/sim_recorder/grapher.py
@@ -4,6 +4,7 @@ from os import walk
 import re
 from collections import defaultdict
 import numpy as np
+from scipy.interpolate import make_interp_spline, BSpline
 
 f = []
 exclude = []
@@ -27,30 +28,47 @@ procs = defaultdict(lambda: defaultdict(list))
 
 for key,el in types.items():
     for name in el:    
+        existing = []
         with open(name) as f:
             for lines in f.readlines():
                 line = lines.split(",")
                 p = line[0]
                 val = line[1:]
                 val = [float(x) for x in val]
+                counter = 0
+                new_p = p
+                while new_p in existing:
+                    counter += 1
+                    new_p = new_p + "_" + str(counter)
+                if counter != 0:
+                    p = p+"_"+str(counter)
                 procs[key][p].append(val)
+                existing.append(p)
         
 for axs, (type, proc) in zip(axs, procs.items()):
     for name, ls in proc.items():
         length = max(map(len, ls))
         arr=np.array([xi+[np.nan]*(length-len(xi)) for xi in ls])
 
-
         mean = np.nanmean(arr, axis=0)
+        standard_dev = np.std(arr, axis=0)
+        print(name, standard_dev)
         x = np.arange(0, mean.shape[0],1)/10 # because recording every 100 ms
-        axs.plot(x, mean, label=name)
+        xnew = np.linspace(x.min(), x.max(), mean.shape[0]*10) 
+
+        spl = make_interp_spline(x, mean, k=3)  # type: BSpline
+        mean_smooth = spl(xnew)
+        #axs.plot(x, mean, label=name)
+        axs.plot(xnew, mean_smooth, label=name)
+
+        axs.fill_between(x, mean-standard_dev, mean+standard_dev, alpha = 0.5, interpolate=True)
         axs.set_xlabel("Time (s)")
         if type == "ram":
             axs.set_ylabel("RAM usage (Mb)")
             axs.set_title("RAM usage against time")
         else:
             axs.set_title("CPU usage against time")
-            axs.set_ylabel("CPU Usage (%)")
+            axs.set_ylabel("CPU Usage (% of one core)")
         axs.legend(bbox_to_anchor=(1,1), loc="upper left")
 plt.subplots_adjust(left=0.05, right=0.75)
 plt.show()
