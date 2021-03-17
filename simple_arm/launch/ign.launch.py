b0894517d1c0a32e399f1f475d8fc47547ddd90e
09ubberboy90 <2330834a@student.gla.ac.uk>
2021-03-17 18:35:32 +0100

    Updating ign controller

--- simple_arm/launch/ign.launch.py
+++ simple_arm/launch/ign.launch.py
@@ -11,81 +11,54 @@ from launch_ros.actions import Node
 import xacro
 
 
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
 def generate_launch_description():
     pkg_name = "simple_arm"
     pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
     pkg_share = get_package_share_directory(pkg_name)
     config_name = "ur_configs"
     config_share = get_package_share_directory(config_name)
+    world = os.path.join(pkg_share,
+                         'worlds', 'panda_throw.sdf')
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_ign_gazebo, 'launch', 'ign_gazebo.launch.py'),
+            launch_arguments=('ign_args', [world, ' -r'])
         ),
     )
-
-
-    xacro_file = os.path.join(config_share,
-                              'urdf','panda',
-                              'panda_arm_hand.urdf.xacro')
-
-    urdf = xacro.process(xacro_file)
-    params = {'robot_description': urdf}
-
-    ign_pub = Node(
-        package=pkg_name,
-        executable='ign_publish',
-        output='screen',
-    )
-    # Follow node
+    robot_description_config = load_file(
+        'ur_configs', 'urdf/panda/panda_arm_hand.urdf')
+    robot_description = {'robot_description': robot_description_config}
     state = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
-
-    )
-    # Follow node
-    joint = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        output='screen',
-        parameters=[params]
-    )
-    spawn = Node(package='ros_ign_gazebo', executable='create',
-                 arguments=[
-                     '-name', 'panda',
-                     '-x', '0.0',
-                     '-z', '0',
-                     '-Y', '0',
-                     '-file', "/home/ubb/Documents/PersonalProject/VrController/ur_configs/urdf/panda/panda_arm_hand.sdf"], 
-                 output='screen')
-
-    joint_names = ["panda_joint1",
-                   "panda_joint2",
-                   "panda_joint3",
-                   "panda_joint4",
-                   "panda_joint5",
-                   "panda_joint6",
-                   "panda_joint7"]
-    # args = [f"/model/ur10/joint/{name}/0/cmd_pos@std_msgs/msg/Float32@ignition.msgs.Float" for name in joint_names]
-    args = [
-        f"/robot/{name}@std_msgs/msg/Float64@ignition.msgs.Double" for name in joint_names]
-    # Bridge
-    bridge = Node(
-        package='ros_ign_bridge',
-        executable='parameter_bridge',
-        arguments=args,
-        output='screen'
+        parameters=[robot_description]
     )
 
-    # RViz
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        # arguments=['-d', os.path.join(pkg_share, 'rviz', 'dolly_ignition.rviz')],
-        condition=IfCondition(LaunchConfiguration('rviz'))
-    )
+    ign_pub = Node(package='simple_arm_control',
+                   executable='republisher',
+                   output='screen',
+                   )
+    # spawn = Node(package='ros_ign_gazebo', executable='create',
+    #              arguments=[
+    #                  '-name', 'panda',
+    #                  '-x', '0.0',
+    #                  '-z', '0',
+    #                  '-Y', '0',
+    #                  '-file', "/home/ubb/Documents/PersonalProject/VrController/ur_configs/urdf/panda/panda_arm_hand.sdf"],
+    #              output='screen')
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -96,9 +69,6 @@ def generate_launch_description():
                               description='Open RViz.'),
         gazebo,
         ign_pub,
-        spawn,
-        #joint,
+        # spawn,
         state,
-        bridge,
-        # rviz
     ])
--- simple_arm_controller/src/ign_controller.cpp
+++ simple_arm_controller/src/ign_controller.cpp
@@ -15,133 +15,86 @@
  *
 */
 
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float64.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 #include <atomic>
 #include <chrono>
 #include <csignal>
+#include <ignition/msgs.hh>
+#include <ignition/transport.hh>
 #include <iostream>
 #include <string>
 #include <thread>
-#include <ignition/msgs.hh>
-#include <ignition/transport.hh>
-
-/// \brief Flag used to break the publisher loop and terminate the program.
-static std::atomic<bool> g_terminatePub(false);
-std::map<std::string, ignition::transport::Node::Publisher> joint_pub;
-std::map<std::string, std::function<void(const ignition::msgs::Double&)>> func_map;
-//////////////////////////////////////////////////
-/// \brief Function callback executed when a SIGINT or SIGTERM signals are
-/// captured. This is used to break the infinite loop that publishes messages
-/// and exit the program smoothly.
-void signal_handler(int _signal)
-{
-  if (_signal == SIGINT || _signal == SIGTERM)
-    g_terminatePub = true;
-}
 
 
-void publish_joint_name(std::string joint_name,const ignition::msgs::Double &_msg)
+class Republisher : public rclcpp::Node
 {
-  std::string topic = "/model/panda/joint/"+joint_name+"/0/cmd_pos";
-  std::cout << "Msg: " << _msg.data() << std::endl << std::endl;
-  if (!joint_pub[joint_name].Publish(_msg))
-  {
-    std::cout << "Failed to publish"<< std::endl;
-  }
-}
-//////////////////////////////////////////////////
-/// \brief Function called each time a topic update is received.
-void panda_joint1(const ignition::msgs::Double &_msg)
-{
-  publish_joint_name("panda_joint1", _msg);
-}
-//////////////////////////////////////////////////
-/// \brief Function called each time a topic update is received.
-void panda_joint2(const ignition::msgs::Double &_msg)
-{
-  publish_joint_name("panda_joint2", _msg);
-}
-//////////////////////////////////////////////////
-/// \brief Function called each time a topic update is received.
-void panda_joint3(const ignition::msgs::Double &_msg)
-{
-  publish_joint_name("panda_joint3", _msg);
-}
-//////////////////////////////////////////////////
-/// \brief Function called each time a topic update is received.
-void panda_joint4(const ignition::msgs::Double &_msg)
-{
-  publish_joint_name("panda_joint4", _msg);
-}
-//////////////////////////////////////////////////
-/// \brief Function called each time a topic update is received.
-void panda_joint5(const ignition::msgs::Double &_msg)
-{
-  publish_joint_name("panda_joint5", _msg);
-}
-//////////////////////////////////////////////////
-/// \brief Function called each time a topic update is received.
-void panda_joint6(const ignition::msgs::Double &_msg)
-{
-  publish_joint_name("panda_joint6", _msg);
-}
-//////////////////////////////////////////////////
-/// \brief Function called each time a topic update is received.
-void panda_joint7(const ignition::msgs::Double &_msg)
-{
-  publish_joint_name("panda_joint7", _msg);
-}
+public:
+    Republisher()
+        : Node("republisher")
+    {
+        subscription_ = this->create_subscription<sensor_msgs::msg::JointState>(
+            "/joint_states", 10, std::bind(&Republisher::listener_callback, this, std::placeholders::_1));
+    }
+    std::map<std::string, ignition::transport::Node::Publisher> joint_pub;
 
+private:
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subscription_;
 
-//////////////////////////////////////////////////
-/// \brief Function called each time a topic update is received.
-void cb(const ignition::msgs::Double &_msg)
-{
-  std::cout << "Msg: " << _msg.data() << std::endl << std::endl;
+    void listener_callback(const sensor_msgs::msg::JointState::SharedPtr msg)
+    {
+        for (int i = 0; i < msg->name.size(); i++)
+        {
+            auto joint_name = msg->name[i];
+            auto position = msg->position[i];
+            publish_joint_name(joint_name, position);
+        }
+        
+    }
 
-}
+    void publish_joint_name(std::string joint_name, double position)
+    {
+        ignition::msgs::Double _msg;
+        std::string topic = "/model/panda/joint/" + joint_name + "/0/cmd_pos";
+        _msg.set_data(position);
+        if (!joint_pub[joint_name].Publish(_msg))
+        {
+            std::cout << "Failed to publish" << std::endl;
+        }
+    }
+};
 
 
 //////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
-  // Install a signal handler for SIGINT and SIGTERM.
-  std::signal(SIGINT,  signal_handler);
-  std::signal(SIGTERM, signal_handler);
+    // Install a signal handler for SIGINT and SIGTERM.
+    rclcpp::init(argc, argv);
 
-  func_map = {
-    {"panda_joint1",panda_joint1},
-    {"panda_joint2",panda_joint2},
-    {"panda_joint3",panda_joint3},
-    {"panda_joint4",panda_joint4},
-    {"panda_joint5",panda_joint5},
-    {"panda_joint6",panda_joint6},
-    {"panda_joint7",panda_joint7},
-  };
-  std::vector<std::string> joint_name = {"panda_joint1",
-                   "panda_joint2",
-                   "panda_joint3",
-                   "panda_joint4",
-                   "panda_joint5",
-                   "panda_joint6",
-                   "panda_joint7"};
-  ignition::transport::Node node_list[joint_name.size()];
-  for (int i=0; i<joint_name.size(); i++) 
-  {  
+    auto republisher = std::make_shared<Republisher>();
+    std::vector<std::string> joint_name = {"panda_joint1",
+                                           "panda_joint2",
+                                           "panda_joint3",
+                                           "panda_joint4",
+                                           "panda_joint5",
+                                           "panda_joint6",
+                                           "panda_joint7",
+                                           "panda_finger_joint1",
+                                           "panda_finger_joint2",};
 
-    auto topic_name = joint_name[i];
-    ignition::transport::Node pub;
-    std::string topic = "/model/panda/joint/"+topic_name+"/0/cmd_pos";
-    joint_pub[topic_name] = pub.Advertise<ignition::msgs::Double>(topic);
-
-    if (!node_list[0].Subscribe("/robot/"+topic_name,func_map[topic_name] ))
+    for (int i = 0; i < joint_name.size(); i++)
     {
-      std::cerr << "Error subscribing to topic [" << topic_name << "]" << std::endl;
-      return -1;
+
+        auto topic_name = joint_name[i];
+        ignition::transport::Node pub;
+        std::string topic = "/model/panda/joint/" + topic_name + "/0/cmd_pos";
+        republisher->joint_pub[topic_name] = pub.Advertise<ignition::msgs::Double>(topic);
     }
+    // Zzzzzz.
+    rclcpp::spin(republisher);
+    ignition::transport::waitForShutdown();
+    rclcpp::shutdown();
 
-  }
-  // Zzzzzz.
-  ignition::transport::waitForShutdown();
-
-  return 0;
+    return 0;
 }
